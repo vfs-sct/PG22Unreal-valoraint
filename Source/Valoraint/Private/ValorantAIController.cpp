3ec6998e67ac058ae64ab838977ce04780f781b8
Shatru <pg22shatrujit@vfs.com>
2022-08-24 14:50:25 -0700

    Deleted unused assets

--- Source/Valoraint/Private/ValorantAIController.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ValorantAIController.h"
-
-AValorantAIController::AValorantAIController()
-{
-	bAttachToPawn = true;
-}
